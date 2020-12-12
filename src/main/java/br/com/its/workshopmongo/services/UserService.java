cb0790418aabbaea0ed176fb35f62a165e392300
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-12-12 05:54:03 -0300

    Insercao de usuario com POST

--- src/main/java/br/com/its/workshopmongo/services/UserService.java
+++ src/main/java/br/com/its/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.its.workshopmongo.domain.User;
+import br.com.its.workshopmongo.dto.UserDTO;
 import br.com.its.workshopmongo.repository.UserRepository;
 import br.com.its.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());		
 	}
 }
