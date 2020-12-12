121cde4d23a9ade5175c3b9d96cbd58d9e53b1d1
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-12-12 06:20:09 -0300

    Atualizacao de usuario com PUT

--- src/main/java/br/com/its/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/its/workshopmongo/resources/UserResource.java
@@ -45,10 +45,17 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 
-
 	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+		User obj = userService.fromDTO(objDTO);
+		obj.setId(id);
+		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/its/workshopmongo/services/UserService.java
+++ src/main/java/br/com/its/workshopmongo/services/UserService.java
@@ -35,7 +35,18 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());		
+	}
+	
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
